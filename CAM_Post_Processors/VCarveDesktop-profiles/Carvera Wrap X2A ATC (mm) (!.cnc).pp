a7263e79fe49fc106521f125d82a4f7a9ae886c5
ACaesuraIsStillMusic <faecorrigandesign@gmail.com>
2024-10-31 16:26:54 -0700

    Initial Commit of source files from makera's releases

---
+++ CAM_Post_Processors/VCarveDesktop-profiles/Carvera Wrap X2A ATC (mm) (!.cnc).pp
@@ -0,0 +1,164 @@
++ =======================================
++ ---
++ Version 1
++   Josh     18/11/2022 Written
++ =======================================
+
+
+POST_NAME = "Carvera Wrap X2A ATC (mm) (*.cnc)"
+
+
+FILE_EXTENSION = "cnc"
+
+
+UNITS = "MM"
+
+
+ROTARY_WRAP_Y = "-A"
+
+
++------------------------------------------------
++    Line terminating characters
++------------------------------------------------
+
+LINE_ENDING = "[13][10]"
+
++------------------------------------------------
++    Block numbering
++------------------------------------------------
+
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+
++================================================
++
++    Formating for variables
++
++================================================
+
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.3]
+VAR Y_POSITION = [Y|C|Y|1.3]
+VAR Z_POSITION = [Z|C|Z|1.3]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.3]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.3]
+VAR WRAP_DIAMETER = [WRAP_DIA|A||1.3]
+
+
+
++================================================
++
++    Block definitions for toolpath output
++
++================================================
+
+
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+
+begin HEADER
+
+"(File created: [DATE] - [TIME])"
+"(for Carvera from Vectric)"
+"G90G54"
+"G17"
+"G21"
+""
+"T[T]M6"
+"G0[ZH]"
+"G0[XH][YH]"
+"[S]M3"
+
+
++---------------------------------------------------
++  Commands output at toolchange
++---------------------------------------------------
+
+begin TOOLCHANGE
+
+"M5"
+""
+"T[T]M6"
+"[S]M3"
+
+
++---------------------------------------------------
++  Commands output for rapid moves
++---------------------------------------------------
+
+begin RAPID_MOVE
+
+"G0[X][Y][Z]"
+
+
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+
+begin FIRST_FEED_MOVE
+
+"G1[X][Y][Z][F]"
+
+
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+
+begin FEED_MOVE
+
+"[X][Y][Z]"
+
++---------------------------------------------------
++  Commands output for the first clockwise arc move
++---------------------------------------------------
+
+begin FIRST_CW_ARC_MOVE
+
+"G2[X][Y][I][J][F]"
+
+
++---------------------------------------------------
++  Commands output for clockwise arc  move
++---------------------------------------------------
+
+begin CW_ARC_MOVE
+
+"G2[X][Y][I][J]"
+
+
++---------------------------------------------------
++  Commands output for the first counterclockwise arc move
++---------------------------------------------------
+
+begin FIRST_CCW_ARC_MOVE
+
+"G3[X][Y][I][J][F]"
+
+
++---------------------------------------------------
++  Commands output for counterclockwise arc  move
++---------------------------------------------------
+
+begin CCW_ARC_MOVE
+
+"G3[X][Y][I][J]"
+
+
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+
+begin FOOTER
+
+"M5"
+"G0[ZH]"
+"G0[XH][YH]"
+"G28"
+"M30"
